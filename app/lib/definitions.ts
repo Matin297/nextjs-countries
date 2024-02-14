c90b56c014211505827641cfb7b66d360b7c831a
Matin Mir <asmamir297@gmail.com>
2024-02-14 12:35:50 +0330

    lib: add country definition based on placeholder data from frontend mentor

---
+++ app/lib/definitions.ts
@@ -0,0 +1,64 @@
+export type Country = {
+  name: string;
+  topLevelDomain: string[];
+  alpha2Code: string;
+  alpha3Code: string;
+  callingCodes: string[];
+  capital: string;
+  altSpellings: string[];
+  subregion: string;
+  region: string;
+  population: number;
+  latlng: [number, number];
+  demonym: string;
+  area: number;
+  timezones: string[];
+  borders: string[];
+  nativeName: string;
+  numericCode: string;
+  flag: string;
+  cioc: string;
+  independent: boolean;
+  flags: Flags;
+  translations: Translations;
+  currencies: Currency[];
+  languages: Language[];
+  regionalBlocs: Bloc[];
+};
+
+type Currency = {
+  code: string;
+  name: string;
+  symbol: string;
+};
+
+type Language = {
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+};
+
+type Flags = {
+  svg: string;
+  png: string;
+};
+
+type Translations = {
+  br: string;
+  pt: string;
+  nl: string;
+  hr: string;
+  fa: string;
+  de: string;
+  es: string;
+  fr: string;
+  ja: string;
+  it: string;
+  hu: string;
+};
+
+type Bloc = {
+  acronym: string;
+  name: string;
+};
