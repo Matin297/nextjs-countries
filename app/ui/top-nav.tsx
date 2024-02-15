df85d6033195a20e4379d35b073e8f25ae7f45b5
Matin Mir <asmamir297@gmail.com>
2024-02-15 19:39:12 +0330

    ui: adjust theme button font/icon size

--- app/ui/top-nav.tsx
+++ app/ui/top-nav.tsx
@@ -4,8 +4,8 @@ export default function TopNav() {
   return (
     <nav className="flex justify-between items-center px-3 py-5 md:px-16">
       <h1 className="font-extrabold">Where in the world?</h1>
-      <button className="flex gap-3 items-center">
-        <MoonIcon className="w-5 h-5" />
+      <button className="flex gap-2 items-center text-sm">
+        <MoonIcon className="w-4 h-4" />
         <span className="font-semibold">Dark Theme</span>
       </button>
     </nav>
