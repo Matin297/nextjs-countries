9f645a245faa81b52e7f3ab2878c5a8b8dd7e416
Matin Mir <asmamir297@gmail.com>
2024-02-17 11:52:02 +0330

    home-page: add country details section

--- app/ui/home/countries-grid.tsx
+++ app/ui/home/countries-grid.tsx
@@ -7,15 +7,34 @@ export default function CountriesGrid() {
       {Array(10)
         .fill(1)
         .map((i) => (
-          <li className="rounded shadow overflow-hidden" key={i}>
+          <li className="rounded shadow overflow-hidden bg-white" key={i}>
             <Link href="/country/1">
               <Image
                 className="w-full"
-                src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_the_Taliban.svg"
+                src="https://flagcdn.com/al.svg"
                 alt="country name flag"
                 width={300}
                 height={150}
               />
+
+              <section className="p-6">
+                <h2 className="mb-6 font-extrabold">Country Name</h2>
+
+                <div>
+                  <span className="font-semibold mr-2">Population:</span>
+                  <span className="text-gray-500">
+                    {(23000000).toLocaleString()}
+                  </span>
+                </div>
+                <div>
+                  <span className="font-semibold mr-2">Region:</span>
+                  <span className="text-gray-500">Europe</span>
+                </div>
+                <div>
+                  <span className="font-semibold mr-2">Capital:</span>
+                  <span className="text-gray-500">Berlin</span>
+                </div>
+              </section>
             </Link>
           </li>
         ))}
