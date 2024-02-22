e82e537821ad0a30d8ac9f18bf1c397cb95c3a28
Matin Mir <asmamir297@gmail.com>
2024-02-22 17:34:36 +0330

    home-ui: create a skeleton for counties list

---
+++ app/ui/home/countries-skeleton.tsx
@@ -0,0 +1,20 @@
+export default function CountriesSkeleton() {
+  return (
+    <ul className="animate-pulse grid grid-cols-auto-fill gap-12">
+      {Array(10)
+        .fill("")
+        .map((_, i) => (
+          <li className="rounded shadow overflow-hidden bg-white" key={i}>
+            <div className="w-full h-[250px] bg-slate-200"></div>
+
+            <div className="p-6">
+              <div className="mb-6 bg-slate-200 w-1/3 h-[20px]"></div>
+              <div className="mb-2 bg-slate-200 w-2/3 h-[15px]"></div>
+              <div className="mb-2 bg-slate-200 w-2/3 h-[15px]"></div>
+              <div className="bg-slate-200 w-2/3 h-[15px]"></div>
+            </div>
+          </li>
+        ))}
+    </ul>
+  );
+}
