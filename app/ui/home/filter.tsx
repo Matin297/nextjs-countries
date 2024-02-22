31df5e05db9ed25359432f005f4c7d287332679b
Matin Mir <asmamir297@gmail.com>
2024-02-22 18:13:07 +0330

    home-ui: keep url search params and select input in sync

--- app/ui/home/filter.tsx
+++ app/ui/home/filter.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import { Region } from "@/app/lib/definitions";
 
 export default function Filter({ regions }: { regions: Region[] }) {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const searchParams = useSearchParams();
+
+  function handleChange(region: string) {
+    const params = new URLSearchParams(searchParams);
+
+    if (region) {
+      params.set("region", region);
+    } else {
+      params.delete("region");
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <select
-      defaultValue=""
+      defaultValue={searchParams.get("region") || ""}
       className="shadow rounded p-3 pr-12 border-0 text-sm"
+      onChange={(e) => handleChange(e.target.value)}
     >
       <option disabled value="">
         Filter by region
