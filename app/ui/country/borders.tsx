6c5d762b32095d8d26fe89147dca5d124b6260e2
Matin Mir <asmamir297@gmail.com>
2024-02-24 17:30:35 +0330

    country-ui: fetch and display border countries as clickable links

---
+++ app/ui/country/borders.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { fetchCountryBorders } from "@/app/lib/data";
+
+export default async function Borders({ countryId }: { countryId: string }) {
+  const borders = await fetchCountryBorders(countryId);
+
+  return (
+    <div className="text-sm flex flex-wrap items-center gap-2">
+      <span className="font-semibold whitespace-nowrap">Border Countries:</span>
+      <ul className="flex flex-wrap gap-2">
+        {borders.map(({ name, id }) => (
+          <li key={id} className="bg-white shadow rounded p-1">
+            <Link href={`/country/${id}`}>{name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
