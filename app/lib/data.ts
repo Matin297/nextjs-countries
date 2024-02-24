98ff244b5e57a727dd1816c886732f53d2614862
Matin Mir <asmamir297@gmail.com>
2024-02-24 15:29:30 +0330

    lib: write fetch country by id sql query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,5 +1,5 @@
 import { sql } from "@vercel/postgres";
-import { Region, CountrySummary } from "@/app/lib/definitions";
+import { Region, CountrySummary, CountryDetails } from "@/app/lib/definitions";
 
 export async function fetchRegions() {
   try {
@@ -36,5 +36,32 @@ export async function fetchCountries(query: string, region: string) {
   } catch (error) {
     console.error("Database error:", error);
     throw new Error("Failed to fetch counties data.");
+  }
+}
+
+export async function fetchCountryById(id: string) {
+  try {
+    const data = await sql<CountryDetails>`
+      SELECT
+        countries.id,
+        countries.name AS "name",
+        regions.name AS "region",
+        population,
+        capital,
+        flag,
+        nativename,
+        subregion,
+        borders,
+        topleveldomain,
+        currencies,
+        languages
+      FROM countries
+      JOIN regions ON regions.id = countries.region_id
+      WHERE countries.id = ${id}
+    `;
+    return data.rows[0];
+  } catch (error) {
+    console.error("Database error:", error);
+    throw new Error("Failed to fetch country data.");
   }
 }
