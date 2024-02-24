d550fec436ae29f4ad17ebfa80704101b5369896
Matin Mir <asmamir297@gmail.com>
2024-02-24 15:30:59 +0330

    country-page: fetch country by id and display its details

--- app/country/[id]/page.tsx
+++ app/country/[id]/page.tsx
@@ -1,7 +1,36 @@
-export default function Page() {
+import Link from "next/link";
+import Image from "next/image";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import Details from "@/app/ui/country/details";
+import { fetchCountryById } from "@/app/lib/data";
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const country = await fetchCountryById(params.id);
+
   return (
-    <main>
-      <h1>Country Page</h1>
+    <main className="py-8 px-6 md:px-16 md:py-12 max-w-6xl">
+      <Link
+        href="/"
+        className="mb-16 py-2 px-6 inline-flex items-center gap-2 shadow rounded text-sm bg-white"
+      >
+        <ArrowLeftIcon className="h-4 w-4" />
+        <span>Back</span>
+      </Link>
+
+      <section className="flex flex-col gap-10 md:flex-row md:gap-16">
+        <Image
+          className="h-[300px] object-cover"
+          src={country.flag}
+          alt={`${country.name}'s flag`}
+          width={400}
+          height={200}
+        />
+
+        <div className="grow">
+          <h2 className="font-extrabold text-xl">{country.name}</h2>
+          <Details country={country} />
+        </div>
+      </section>
     </main>
   );
 }
