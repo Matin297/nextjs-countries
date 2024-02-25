d00ab5ab94d49bbd8281c322c9caa7cc12f8f7ed
Matin Mir <asmamir297@gmail.com>
2024-02-25 15:16:53 +0330

    home-ui: match countries grid with skeleton

--- app/ui/home/countries-skeleton.tsx
+++ app/ui/home/countries-skeleton.tsx
@@ -8,13 +8,13 @@ export default function CountriesSkeleton() {
             className="rounded shadow overflow-hidden bg-white dark:bg-gray-700"
             key={i}
           >
-            <div className="w-full h-[250px] bg-slate-200 dark:bg-slate-300"></div>
+            <div className="w-full h-[180px] bg-slate-200 dark:bg-slate-300"></div>
 
             <div className="p-6">
-              <div className="mb-6 bg-slate-200 dark:bg-slate-300 w-1/3 h-[20px]"></div>
-              <div className="mb-2 bg-slate-200 dark:bg-slate-300 w-2/3 h-[15px]"></div>
-              <div className="mb-2 bg-slate-200 dark:bg-slate-300 w-2/3 h-[15px]"></div>
-              <div className="bg-slate-200 dark:bg-slate-300 w-2/3 h-[15px]"></div>
+              <div className="mb-6 bg-slate-200 dark:bg-slate-300 rounded w-1/3 h-[24px]"></div>
+              <div className="mb-2 bg-slate-200 dark:bg-slate-300 rounded w-2/3 h-[20px]"></div>
+              <div className="mb-2 bg-slate-200 dark:bg-slate-300 rounded w-2/3 h-[20px]"></div>
+              <div className="bg-slate-200 dark:bg-slate-300 rounded w-2/3 h-[20px]"></div>
             </div>
           </li>
         ))}
